31605d9e27e9cb267547cfac929dccf96bb9b6b0
Apollo Tang <tang.apollo@gmail.com>
2019-12-14 01:42:46 -0500

    added library

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var uniqueRandomArray = require('unique-random-array')
+var starWarNames = require('./starwars-names.json')
+
+
+module.exports = {
+  all: starWarNames,
+  random: uniqueRandomArray(starWarNames)
+}
+
