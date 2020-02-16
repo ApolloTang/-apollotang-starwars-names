ddbcd4d4574be5dffb3a2641d439887a0af7d340
Apollo Tang <tang.apollo@gmail.com>
2020-02-15 21:57:10 -0500

    add jest.config.js; add test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,20 @@
 var starWars = require('./index')
 
 describe('starwars-names', () => {
-  it('should work!', () => {
-    expect(true).toBe(true)
+  describe('all', () => {
+    it('should be an array of strings', () => {
+      expect(starWars.all)
+        .toSatisfyAll( starWarsNames => typeof starWarsNames === 'string')
+    })
+
+    it('should contain `Luke Skywalker`', () => {
+      expect(starWars.all).toContain('Luke Skywalker')
+    })
+  })
+
+  describe('random', () => {
+    it('should return a random item form the starWars.all', ()=>{
+      expect(starWars.all).toContain(starWars.random())
+    })
   })
 })
